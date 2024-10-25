ea5b1cf99d375c6bc679d8caba43d5647716b393
Everardo Montes <89950434+EverardoMontes@users.noreply.github.com>
2024-10-25 01:27:36 -0600

    Arreglo de fotografías y varios

--- jsFiles/Modales/modal.js
+++ jsFiles/Modales/modal.js
@@ -14,11 +14,18 @@ window.onload = function () {
     modalErrores: `https://www.youtube.com/embed/OUSj2zRb_EE?si=8clQQSZxKd1etYq_`,
     modalConociéndonosEntreVecinos: ``,
     modalLlegaronNuevosVecinos: ``,
+    modalObservaALosPersonajesYPracticaSusNombres: `https://www.youtube.com/embed/wDuisQBH8pY?si=AKLyZ0tNCDKcA-uo`,
+    modalObservaYPracticaYoSoyParaPresentarte: `https://www.youtube.com/embed/Lb7hc9woHtk?si=PGEp788E8Me9i5M5`,
     modalJuguemosMemorama: `https://www.youtube.com/embed/_Vt7l2mDveA?si=juDhdXKq0CZU3CJW`,
-    modalYoSoy: ``,
-    modalNuevasFormasDePresentarnos: ``,
+    modalYoSoy: `https://www.youtube.com/embed/P4QdT7QfqAE?si=eAUOLRsUJyAYgstm`,
+    modalNuevasFormasDePresentarnos: `https://www.youtube.com/embed/LRRLQZhCwBs?si=4oNFunX9qC0XkrOc`,
     modalRegresar: `https://www.youtube.com/embed/pV3O0vJA_84?si=DYpgJJZQIbpGbotV`,
     modalSiguiente: `https://www.youtube.com/embed/WHciLN4oqZk?si=ouUWc4obec4CltLn`,
+    modalOrdenaLasLetrasDeCadaNombre:`https://www.youtube.com/embed/GVjSW21ZPBk?si=BojeQmwz6an4Wfdx`,
+    modalSoyJavier:``,
+    modalSoyTeresa:``,
+    modalSoyLuis:``,
+    modalSoyPablo:``,
   };
   // Al hacer clic en el botón, mostrar el modal
   buttons.forEach((button) => {
